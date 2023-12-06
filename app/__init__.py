d7decf619a975739150c6352795ad5065c8850e1
Michael Phan <michaelalexanderphan@gmail.com>
2023-12-05 17:55:49 -0800

    jwtauth

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 # Initialize the Flask app
 app = Flask(__name__)
 
-# Configurations for your Flask app, such as database URI and secret key
+# Set up SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flatironfit.db'
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-
-# Initialize the SQLAlchemy ORM
 db = SQLAlchemy(app)
 
-# Import models and routes below the SQLAlchemy instance creation
+# Set up JWT
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'  # Change this!
+jwt = JWTManager(app)
+
+# Set up Flask-Migrate
+migrate = Migrate(app, db)
+
+# Import models
 from app.models.user import User
 from app.models.workout import Workout
 
 # Import routes modules and register Blueprints
 from app.routes import user_routes, workout_routes
 
-# Registering the user and workout blueprints to handle respective routes
 app.register_blueprint(user_routes.user_bp)
 app.register_blueprint(workout_routes.workout_bp)
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -15,4 +15,45 @@ def create_user():
     # Logic to create a new user
     pass
 
+@user_bp.route('/register', methods=['POST'])
+def register():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    
+    if User.query.filter_by(username=username).first():
+        return jsonify({"msg": "Username already exists"}), 409
+
+    new_user = User(username=username)
+    new_user.set_password(password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"msg": "User created successfully"}), 201
+
+@user_bp.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    
+    if not username or not password:
+        return jsonify({"msg": "Missing username or password"}), 400
+    
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token), 200
+    else:
+        return jsonify({"msg": "Bad username or password"}), 401
+
 # Add more user-related routes such as login, profile update, etc.
