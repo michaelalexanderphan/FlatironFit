acc955dd7cb4742a611e54ee574d248fd009e741
Michael Phan <michaelalexanderphan@gmail.com>
2023-12-10 23:11:43 -0800

    starting front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './views/Home';
+import Login from './views/Login';
+import Signup from './views/Signup';
+import NotFound from './views/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Signup() {
+  return (
+    <div>
+      <h2>Signup</h2>
+      {/* Signup form will go here */}
+    </div>
+  );
+}
+
+export default Signup;
