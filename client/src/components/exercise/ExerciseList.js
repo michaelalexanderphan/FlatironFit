f618ecdea900c9a6d17ed4a86478cd57851d6f34
Michael Phan <michaelalexanderphan@gmail.com>
2023-12-14 00:16:34 -0800

    changes

--- client/src/components/exercise/ExerciseList.js
+++ client/src/components/exercise/ExerciseList.js
@@ -4,28 +4,39 @@ import axios from 'axios';
 function Exercises() {
   const [exercises, setExercises] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    const fetchExercises = async () => {
-      try {
-        const response = await axios.get('/api/exercises');
-        setExercises(response.data);
-      } catch (error) {
-        console.error('Error fetching exercises', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
     fetchExercises();
   }, []);
+
+  const fetchExercises = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.get('/api/exercises');
+      setExercises(response.data);
+      setError('');
+    } catch (error) {
+      setError('Failed to fetch exercises');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const deleteExercise = (exerciseId) => {
+    axios.delete(`/api/exercises/${exerciseId}`)
+      .then(() => {
+        fetchExercises(); 
+      })
+      .catch(error => {
+        console.error('Error deleting exercise', error);
+      });
+  };
 
   return (
     <div>
       <h2>Exercise List</h2>
-      {isLoading ? (
-        <p>Loading exercises...</p>
-      ) : (
+      {isLoading ? <p>Loading exercises...</p> : error ? <p>{error}</p> : (
         <ul>
           {exercises.map(exercise => (
             <li key={exercise.id}>
@@ -33,7 +44,10 @@ function Exercises() {
               <p>{exercise.description}</p>
               <p>Body Part: {exercise.body_part}</p>
               <p>Difficulty: {exercise.difficulty}</p>
-              {exercise.youtube_url && <a href={exercise.youtube_url} target="_blank" rel="noopener noreferrer">Watch on YouTube</a>}
+              {exercise.youtube_url && (
+                <a href={exercise.youtube_url} target="_blank" rel="noopener noreferrer">Watch on YouTube</a>
+              )}
+              <button onClick={() => deleteExercise(exercise.id)}>Delete</button>
             </li>
           ))}
         </ul>
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useEffect, useContext } from 'react';
 
 export const AuthContext = createContext(null);
 
@@ -7,16 +7,28 @@ export const useAuth = () => useContext(AuthContext);
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const storedUser = sessionStorage.getItem('user');
+    if (storedUser) {
+      try {
+        const userData = JSON.parse(storedUser);
+        setUser(userData);
+      } catch (e) {
+        console.error('Failed to parse user data:', e);
+      }
+    }
+  }, []);
+
   const login = (userData, token) => {
     setUser(userData);
+    sessionStorage.setItem('user', JSON.stringify(userData));
     sessionStorage.setItem('authToken', token);
-    console.log('User logged in:', userData); // Add this line
   };
 
   const logout = () => {
     setUser(null);
+    sessionStorage.removeItem('user');
     sessionStorage.removeItem('authToken');
-    console.log('User logged out'); // Add this line
   };
 
   return (
